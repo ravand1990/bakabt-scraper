8bde6ace926208e77f740708a1df043e186066e3
r.atruschi <r.atruschi@optadata-gruppe.de>
2023-10-18 14:15:24 +0200

    randomness

--- fetchTorrents.ts
+++ fetchTorrents.ts
@@ -1,19 +1,27 @@
-import axios, { AxiosResponse } from 'axios';
-import cheerio from 'cheerio';
-import express from 'express';
-import RSS from 'rss';
+import axios, { AxiosResponse } from "axios";
+import cheerio from "cheerio";
+import express from "express";
+import RSS from "rss";
 import dotenv from "dotenv";
-import randomUA from 'random-useragent';  // For user-agent rotation
+import * as fs from "fs";
+import randomUA from "random-useragent"; // For user-agent rotation
+
+if (!fs.existsSync(".env")) fs.writeFileSync(".env", "cookie=");
 
 dotenv.config(); // Load environment variables from .env file
 
-const BASE_URL: string = 'https://bakabt.me';
+if (!process.env.cookie) {
+  console.log('"cookie" variable not set in .env');
+  process.exit();
+}
+
+const BASE_URL: string = "https://bakabt.me";
 const app = express();
 const port = 3000;
 
 let feed = new RSS({
-  title: 'Freeleech Torrents',
-  description: 'List of freeleech torrents from Bakabt',
+  title: "Freeleech Torrents",
+  description: "List of freeleech torrents from Bakabt",
   feed_url: `http://localhost:${port}/rss`,
   site_url: BASE_URL,
 });
@@ -28,36 +36,46 @@ function getRandomDelay() {
 
 async function updateFeed(): Promise<void> {
   try {
-    const response: AxiosResponse<string> = await axios.get(`${BASE_URL}/browse.php`, {
-      headers: {
-        'User-Agent': randomUA.getRandom(),
-        'Cookie': 'bbtid=' + process.env.cookie
-      },
-      timeout: 5000  // 5 seconds timeout
-    });
+    const response: AxiosResponse<string> = await axios.get(
+      `${BASE_URL}/browse.php`,
+      {
+        headers: {
+          "User-Agent": randomUA.getRandom(),
+          Cookie: "bbtid=" + process.env.cookie,
+        },
+        timeout: 5000, // 5 seconds timeout
+      }
+    );
 
     const $ = cheerio.load(response.data);
 
-    $('td.name').each(async (index, element) => {
-      const freeleechIcon = $(element).find('.icon.freeleech');
+    $("td.name").each(async (index, element) => {
+      const freeleechIcon = $(element).find(".icon.freeleech");
       if (freeleechIcon.length) {
-        const torrentLink = $(element).find('.title').attr('href');
-        const torrentName = $(element).find('.title').text().trim();
-        
+        const torrentLink = $(element).find(".title").attr("href");
+        const torrentName = $(element).find(".title").text().trim();
+
         if (torrentLink && !addedTorrents.has(torrentLink)) {
           const detailPageLink = BASE_URL + torrentLink;
 
-          const torrentPageResponse: AxiosResponse<string> = await axios.get(detailPageLink, {
-            headers: {
-              'User-Agent': randomUA.getRandom(),
-              'Cookie': 'bbtid=' + process.env.cookie
-            },
-            timeout: 5000  // 5 seconds timeout
-          });
-          
+          const torrentPageResponse: AxiosResponse<string> = await axios.get(
+            detailPageLink,
+            {
+              headers: {
+                "User-Agent": randomUA.getRandom(),
+                Cookie: "bbtid=" + process.env.cookie,
+              },
+              timeout: 5000, // 5 seconds timeout
+            }
+          );
+
           const torrentPage$ = cheerio.load(torrentPageResponse.data);
-          const downloadLink = torrentPage$('a[href$=".torrent"]').attr('href');
-          const date = new Date(Number(torrentPage$('span[data-timestamp]').attr('data-timestamp')) * 1000);
+          const downloadLink = torrentPage$('a[href$=".torrent"]').attr("href");
+          const date = new Date(
+            Number(
+              torrentPage$("span[data-timestamp]").attr("data-timestamp")
+            ) * 1000
+          );
 
           if (downloadLink) {
             feed.item({
@@ -66,38 +84,41 @@ async function updateFeed(): Promise<void> {
               url: detailPageLink,
               enclosure: {
                 url: BASE_URL + downloadLink,
-                type: 'application/x-bittorrent'
+                type: "application/x-bittorrent",
               },
-              date: date
+              date: date,
             });
 
-            addedTorrents.add(torrentLink);  // Mark the torrent as added
+            addedTorrents.add(torrentLink); // Mark the torrent as added
           }
 
           // Random delay between each request to the torrent detail pages
-          await new Promise(resolve => setTimeout(resolve, getRandomDelay()));
+          await new Promise((resolve) => setTimeout(resolve, getRandomDelay()));
         }
       }
     });
-
   } catch (error) {
-    console.error('Error updating feed:', error);
+    console.error("Error updating feed:", error);
   }
 }
 
-app.get('/rss', (req, res) => {
-  res.type('application/rss+xml');
+app.get("/", (req, res) => {
+  res.redirect("/rss");
+});
+
+app.get("/rss", (req, res) => {
+  res.type("application/rss+xml");
   res.send(feed.xml());
 });
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
-  
+
   // Using setTimeout for controlled intervals
   async function periodicUpdate() {
     await updateFeed();
-    setTimeout(periodicUpdate, 1000 * 60 * 5 + getRandomDelay());  // Update every 5 minutes + random delay
+    setTimeout(periodicUpdate, 1000 * 60 * 5 + getRandomDelay()); // Update every 5 minutes + random delay
   }
 
-  periodicUpdate();  // Initial call
+  periodicUpdate(); // Initial call
 });
